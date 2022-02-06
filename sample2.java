4f994d2989fe775f8f5992ad71d7d1754d5d4f11
“rohith355” <rohitkarthick355@gmail.com>
2022-02-06 15:41:16 +0000

    gittogithub2

--- sample2.java
+++ sample2.java
@@ -5,6 +5,5 @@ public class MyClass {
     MyClass myObj1 = new MyClass();  // Object 1
     MyClass myObj2 = new MyClass();  // Object 2
     System.out.println(myObj1.x);
-    System.out.println(myObj2.x);
   }
 }
